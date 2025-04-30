dc34a95c9f4d061aa6f11c9521749c9c7c0a5814
Soesep Dev <soesep.dev@gmail.com>
2025-04-30 11:35:35 +0700

    update : app

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-
-const ScrollToTop = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-  }, [location]);
-
-  return null;
-};  
 
 const App = () => { 
   return (
     <BrowserRouter>  
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={ <Home/> } />
       </Routes>
